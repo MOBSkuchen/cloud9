74982e9e1540786b16e72d1f58847160c4d1e43b
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2024-06-22 17:08:23 +0200

    Add an interface for creating clients

---
+++ cloud9client/ClientBlueprint.cs
@@ -0,0 +1,23 @@
+﻿using DokanNet;
+namespace cloud9client;
+
+public interface IClientBlueprint
+{
+    public String ConvertFmt(String previous);
+    public void Disconnect();
+    public bool DeletePath(String path);
+    public FileInformation? ConstructFileInfoMNull(String path);
+    public FileInformation ConstructFileInfo(String path);
+    public FileInformation ConvertFileInfo(object item);
+    public List<FileInformation> ListFiles(String path);
+    public bool FileExists(String path);
+    public int ReadBuffer(String path, byte[] bytes, int length, int offset);
+    public void WriteBuffer(String path, byte[] buffer, int offset);
+    public void SetFileTimes(string path, DateTime? atime, DateTime? mtime);
+    public void CreateFile(String path);
+    public void CreateDirectory(String path);
+    public void MoveFile(String oldpath, String newpath);
+    public void SetFileSize(String path, long size);
+    public FileInformation GetFileInfoCached(String path);
+    public (long totalBytes, long freeBytes) GetDriveSize();
+}
