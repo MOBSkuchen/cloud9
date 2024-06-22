bd48c3025851218fadac8e11bd7f754988b06a47
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2024-06-22 17:11:10 +0200

    Create a structure for instance data

---
+++ cloud9client/InstanceData.cs
@@ -0,0 +1,15 @@
+﻿namespace cloud9client;
+
+public struct InstanceData
+{
+    public String Host;
+    public String Username;
+    public int Port;
+
+    public String Password;
+    public bool IsKeyAuth;
+
+    public String RemotePath;
+    
+    public float ProtocolVersion;
+}
