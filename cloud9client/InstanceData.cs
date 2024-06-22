f12588766b241ba045d35311b9674d90e57e5f7e
MOBSkuchen <71943003+MOBSkuchen@users.noreply.github.com>
2024-06-22 19:54:42 +0200

    Create init and convert methods for InstanceData

--- cloud9client/InstanceData.cs
+++ cloud9client/InstanceData.cs
@@ -2,6 +2,21 @@
 
 public struct InstanceData
 {
+    public InstanceData(String host, String username, int port, String password, bool isKeyAuth, String remotePath, String driveName, String mountPath, double protocolVersion)
+    {
+        Host = host;
+        Username = username;
+        Port = port;
+
+        Password = password;
+        IsKeyAuth = isKeyAuth;
+
+        RemotePath = remotePath;
+        DriveName = driveName;
+        MountPath = mountPath;
+
+        ProtocolVersion = protocolVersion;
+    }
     public String Host;
     public String Username;
     public int Port;
@@ -10,6 +25,32 @@ public struct InstanceData
     public bool IsKeyAuth;
 
     public String RemotePath;
+    public String DriveName;
+    public String MountPath;
     
-    public float ProtocolVersion;
+    public double ProtocolVersion;
+
+    public static InstanceData? ConvertToInstanceData(Dictionary<string, string> instanceDataDictionary)
+    {
+        String[] mustHaveList =
+            ["host", "username", "password", "port", "isKeyAuth", "driveName", "remotePath", "mountPath"];
+
+        foreach (var field in mustHaveList)
+        {
+            if (!instanceDataDictionary.ContainsKey(field)) return null;
+        }
+
+        try {
+            return new InstanceData(instanceDataDictionary["host"], instanceDataDictionary["username"],
+                Convert.ToInt32(instanceDataDictionary["port"]), instanceDataDictionary["password"],
+                instanceDataDictionary["isKeyAuth"] == "true", instanceDataDictionary["remotePath"],
+                instanceDataDictionary["driveName"],
+                instanceDataDictionary["mountPath"], 1.0);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Conversion error");
+            return null;
+        }
+    }
 }
