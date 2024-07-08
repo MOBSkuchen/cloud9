d298410dae7f61a0ce296a604e89d2ec24a5a98d
MOBSkuchen <mobskuchen@gmail.com>
2024-07-08 11:14:04 +0200

    Fully implement CloneDriver

--- cloud9client/CloneDriver.cs
+++ cloud9client/CloneDriver.cs
@@ -17,7 +17,7 @@ public class CloneDriver : IClientBlueprint
         return previous.Replace("\\", "/");
     }
 
-    public void Disconnect() { }
+    public void Close() { }
     
     public static void RecursiveDelete(DirectoryInfo baseDir)
     {
@@ -33,6 +33,7 @@ public class CloneDriver : IClientBlueprint
 
     public bool DeletePath(string path)
     {
+        path = ConvertFmt(path);
         try
         {
             RecursiveDelete(new DirectoryInfo(path));
@@ -44,9 +45,17 @@ public class CloneDriver : IClientBlueprint
         }
     }
 
-    public FileInformation? ConstructFileInfoMNull(String path) { if (!FileExists(path)) return null; return ConstructFileInfo(path);}
-    
-    public FileInformation ConstructFileInfo(String path) {return ConvertFileInfo(new FileInfo(path)); }
+    public FileInformation? ConstructFileInfoMNull(String path)
+    {
+        path = ConvertFmt(path);
+        if (!FileExists(path)) return null; return ConstructFileInfo(path);
+    }
+
+    public FileInformation ConstructFileInfo(String path)
+    {
+        path = ConvertFmt(path);
+        return ConvertFileInfo(new FileInfo(path));
+    }
 
     public FileInformation ConvertFileInfo(object item_)
     {
@@ -64,6 +73,7 @@ public class CloneDriver : IClientBlueprint
 
     public List<FileInformation> ListFiles(string path)
     {
+        path = ConvertFmt(path);
         List<FileInformation> files = new List<FileInformation>();
         foreach (var item in Directory.EnumerateFileSystemEntries(path))
         {
@@ -75,53 +85,73 @@ public class CloneDriver : IClientBlueprint
 
     public bool FileExists(string path)
     {
+        path = ConvertFmt(path);
         return Path.Exists(path);
     }
 
     public int ReadBuffer(string path, byte[] bytes, int length, int offset)
     {
-        throw new NotImplementedException();
+        path = ConvertFmt(path);
+        var readStream = File.OpenRead(path);
+        return readStream.Read(bytes, offset, length);
     }
 
     public void WriteBuffer(string path, byte[] buffer, int offset)
     {
-        throw new NotImplementedException();
+        path = ConvertFmt(path);
+        var writeBuffer = File.OpenWrite(path);
+        writeBuffer.Write(buffer, offset, buffer.Length);
+        writeBuffer.Flush();
     }
 
-    public void SetFileTimes(string path, DateTime? atime, DateTime? mtime)
+    public void SetFileTimes(string path, DateTime? atime, DateTime? mtime, DateTime? ctime)
     {
-        File.SetLastAccessTime();
+        path = ConvertFmt(path);
+        if (atime != null) {File.SetLastAccessTime(path, atime.Value);}
+        if (mtime != null) {File.SetLastWriteTime(path, mtime.Value);}
+        if (ctime != null) {File.SetCreationTime(path, ctime.Value);}
     }
 
     public void CreateFile(string path)
     {
+        path = ConvertFmt(path);
         File.Create(path);
     }
 
     public void CreateDirectory(string path)
     {
+        path = ConvertFmt(path);
         Directory.CreateDirectory(path);
     }
 
     public void MoveFile(string oldpath, string newpath)
     {
+        oldpath = ConvertFmt(oldpath);
+        newpath = ConvertFmt(newpath);
         File.Move(oldpath, newpath);
     }
 
     public void SetFileSize(string path, long size)
     {
+        path = ConvertFmt(path);
         var f = File.Open(path, FileMode.Open);
         f.SetLength(size);
     }
 
     public FileInformation GetFileInfo(string path)
     {
-        var f = File.Open(path, FileMode.Open);
-        f.
+        path = ConvertFmt(path);
+        return ConstructFileInfo(path);
     }
 
     public (long totalBytes, long freeBytes) GetDriveSize()
     {
-        throw new NotImplementedException();
+        return (0, 0);
+    }
+
+    public void SetFileAttributes(string path, FileAttributes fileAttributes)
+    {
+        path = ConvertFmt(path);
+        File.SetAttributes(path, fileAttributes);
     }
 }
